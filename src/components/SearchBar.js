a7754d426bdd5afd6626bc0c59341c735c054353
Mitar Perovic <perovicmitar@gmail.com>
2020-04-25 18:41:19 +0200

    Changed width of search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,22 +8,22 @@ import Grid from '@material-ui/core/Grid';
 
 export const SearchBar = (props) => {
     const [city, setCity] = useState('')
-    
+
 
     return (
-        <Grid container spacing={0} style={{paddingBottom:'35px'}}>
-        <Grid xs={2}>
-        </Grid>
+        <Grid container spacing={0} style={{ paddingBottom: '35px' }}>
+            <Grid xs={2}>
+            </Grid>
             <Grid xs={7}>
-                <TextField value={city} onChange={(e) => setCity(e.target.value)} error={props.error} style={{ width: 500 }} label="Enter the city's name" variant="outlined" />
+                <TextField value={city} onChange={(e) => setCity(e.target.value)} error={props.error} style={{ width: '95%' }} label="Enter the city's name" variant="outlined" />
             </Grid>
             <Grid xs={1}>
-                <Button style={{ marginTop: '8px', marginLeft:'-15px' }} variant="contained" size="medium" color="secondary" onClick={() => { props.handleSubmit(city); setCity(''); }}>Add</Button>
+                <Button style={{ marginTop: '8px', marginLeft: '-15px' }} variant="contained" size="medium" color="secondary" onClick={() => { props.handleSubmit(city); setCity(''); }}>Add</Button>
             </Grid>
             <Grid xs={2}>
+            </Grid>
         </Grid>
-        </Grid>
-       
+
     )
 }
 
