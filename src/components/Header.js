caec9280fa0604b2425c22126f2a0275d43c6148
Mitar Perovic <perovicmitar@gmail.com>
2020-04-27 09:34:43 +0200

    Added a divider

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Divider from '@material-ui/core/Divider';
 
 export default function Header() {
     return (
-        <div className='header' style={{backgroundColor:''}} >
+        <div className='header' style={{ marginBottom: 20, backgroundColor: '' }} >
             <div classnmae='header-logo'>
                 <h2>Weatherly</h2>
             </div>
+            <Divider />
+
         </div>
     )
 }
